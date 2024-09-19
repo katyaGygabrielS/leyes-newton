c83ed01f97437b99db954287a20eb30875d9f645
katyaGygabrielS <ceciliakatya8@gmail.com>
2024-09-18 18:14:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function toggleMenu() {
     const main = document.querySelector('main');
 
     if (menu.style.left === '0px') {
-        menu.style.left = '-220px';
+        menu.style.left = '-240px';
         main.style.marginLeft = '20px';
     } else {
         menu.style.left = '0px';
-        main.style.marginLeft = '220px';
+        main.style.marginLeft = '240px';
     }
 }
