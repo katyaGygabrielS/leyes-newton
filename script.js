fd6c1fcc0e6115f3c421b0839a1ff574797e5b5f
katyaGygabrielS <ceciliakatya8@gmail.com>
2024-09-18 18:36:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Función para mostrar el menú
+// Función para mostrar/ocultar el menú
 function toggleMenu() {
     const menu = document.getElementById('menu');
     const main = document.querySelector('main');
 
     if (menu.style.left === '0px') {
-        menu.style.left = '-240px';
-        main.style.marginLeft = '20px';
+        menu.style.left = '-240px'; // Esconde el menú
+        main.style.marginLeft = '20px'; // Restaura el contenido principal
     } else {
-        menu.style.left = '0px';
-        main.style.marginLeft = '240px';
+        menu.style.left = '0px'; // Muestra el menú
+        main.style.marginLeft = '240px'; // Ajusta el contenido principal
     }
 }
